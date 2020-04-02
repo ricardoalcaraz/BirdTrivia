0df8f83f14e215393371c606aaee770a2990f078
a-ridley <abixcastro@gmail.com>
2020-04-01 18:53:08 -0700

    Added DAL and service layer

---
+++ DataAccessLayer/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ ServiceLayer/UserService.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer;
+using ServiceLayer.Interfaces;
+using System;
+
+namespace ServiceLayer
+{
+    public class UserService : IUserService
+    {
+        public User CreateUser(User user)
+        {
+            // Todo: Add implementation
+            throw new NotImplementedException();
+        }
+
+        public int GetTallyCount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
